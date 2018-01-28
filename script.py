014fd3be09efc95bae944f26be47981bc4d91e14
Paulo Henrique <paulocardoso@infojr.com.br>
2018-01-28 17:15:55 -0300

    add who search

--- script.py
+++ script.py
@@ -1,15 +1,30 @@
 from nltk.tokenize import word_tokenize
 import nltk
 from nltk.corpus import stopwords
+import sparql
 
-
+search = raw_input("Search:")
 result = []
-words = nltk.word_tokenize("who is Caetano Veloso?")
+words = nltk.word_tokenize(search)
 sTwords = stopwords.words('english')
 
 for word in words:
     if word not in sTwords:
         result.append(word)
 
-print(result)
-print(nltk.pos_tag(words))
+tagged_words = nltk.pos_tag(words)
+
+
+#caso seja who
+name = ''
+i = 0
+for word in tagged_words:
+    if (word[1] == 'NNP'):
+        if (i == 0):
+            name += word[0]
+        else:
+            name += ' ' + word[0]
+    
+        i = i + 1
+
+sparql.whois(name)
